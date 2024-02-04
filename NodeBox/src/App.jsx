58801ae8abfa700b7761f0a9d9fe376162c98fb9
Esra Koç <esrakc239@gmail.com>
2024-02-04 22:57:23 +0300

    react

---
+++ NodeBox/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import FlashCards from "./FlashCards";
+
+function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+export default App;
